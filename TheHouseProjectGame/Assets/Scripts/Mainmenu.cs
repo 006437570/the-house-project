872fc6b35898f60acebdb01e8dd12aa1901f1817
andrewadame <67297166+andrewadame@users.noreply.github.com>
2022-12-06 04:03:33 -0800

    Main Menu made

--- TheHouseProjectGame/Assets/Scripts/Mainmenu.cs
+++ TheHouseProjectGame/Assets/Scripts/Mainmenu.cs
@@ -7,7 +7,27 @@ public class Mainmenu : MonoBehaviour
 {
     public void PlayGame()
     {
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void AITest()
+    {
+        SceneManager.LoadScene("AITest");
+    }
+
+    public void BRTest()
+    {
+        SceneManager.LoadScene("BRTest");
+    }
+
+    public void MapRNG()
+    {
+        SceneManager.LoadScene("MapRNGTest");
+    }
+
+    public void RingTest()
+    {
+        SceneManager.LoadScene("RingTest");
     }
 
     public void QuitGame()
